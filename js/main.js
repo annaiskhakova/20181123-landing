174da35536f6c7329430f0e925e494bfe656efb7
annaiskhakova <khvesta1@gmail.com>
2018-12-03 20:18:25 +0300

    2018

--- js/main.js
+++ js/main.js
@@ -140,20 +140,77 @@ for (let i = 0; i < OpenSlaiderBTN.length; i++ ) {
 
 //map
 ymaps.ready(function () {
-var myMap = new ymaps.Map('map', {
-            center: [55.751574, 37.573856],
-            controls: ['zoomControl'],
-            zoom: 9
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        clusterer = new ymaps.Clusterer({
-            preset: 'islands#invertedNightClusterIcons',
-            clusterHideIconOnBalloonOpen: false,
-            geoObjectHideIconOnBalloonOpen: false
-        });
+// var myMap = new ymaps.Map('map', {
+//             center: [55.751574, 37.573856],
+//             controls: ['zoomControl'],
+//             zoom: 9
+//         }, {
+//             searchControlProvider: 'yandex#search'
+//         }),
+//         clusterer = new ymaps.Clusterer({
+//             preset: 'islands#invertedNightClusterIcons',
+//             clusterHideIconOnBalloonOpen: false,
+//             geoObjectHideIconOnBalloonOpen: false
+//         });
 
+// });
+
+var myMap = new ymaps.Map('map', {
+    center: [59.93416159991021,30.32580739998837],
+    zoom: 12
+}, {
+    searchControlProvider: 'yandex#search'
+}),
+
+// Создаём макет содержимого.
+// MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+//     '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+// ),
+
+myPlacemark = new ymaps.Placemark([59.93974397970528,30.331460794753102], {
+    hintContent: 'бургерная1',
+    balloonContent: 'бургерная1'
+}, {
+    iconLayout: 'default#image',
+    iconImageHref: './img/map-marker.svg',
+    iconImageSize: [50, 60],
+    iconImageOffset: [-5, -38]
+
+}
+);
+
+myPlacemark1 = new ymaps.Placemark([59.89837280948496,30.335946705269627], {
+    hintContent: 'бургерная1',
+    balloonContent: 'бургерная1'
+}, {
+    iconLayout: 'default#image',
+    iconImageSize: [50, 60],
+    iconImageHref: './img/map-marker.svg',
+    iconImageOffset: [-5, -38]
+
+}
+);
+
+myPlacemark2 = new ymaps.Placemark([59.94222081313683,30.248579121326642], {
+    hintContent: 'бургерная1',
+    balloonContent: 'бургерная1'
+}, {
+    iconLayout: 'default#image',
+    iconImageSize: [50, 60],
+    iconImageHref: './img/map-marker.svg',
+    iconImageOffset: [-5, -38]
+
+}
+);
+
+
+myMap.geoObjects
+.add(myPlacemark)
+.add(myPlacemark1)
+.add(myPlacemark2)
+//.add(myPlacemarkWithContent);
 });
+
 
 
 
@@ -198,7 +255,7 @@ sendButton.addEventListener('click', event => {
         // const overlayElement = fragment.querySelector(".response__overlay");
         // const contentElement = fragment.querySelector(".response__content");
         // const closeElement = fragment.querySelector(".response__close");
-        console.log( template1);
+        console.log(template1);
         console.log(sendmail);
         template1.classList.add('response__overlay-active');
       
@@ -251,13 +308,14 @@ function validateForm(form) {
 const Review = document.querySelector(".reviews");
 const revButton = document.querySelectorAll(".reviews__btn");
 const revBlock = document.querySelector(".reviews__block");
+const revOverlay =  document.querySelector(".reviews__overlay")
 
 for (let i = 0; i < revButton.length; i++ ) {
     const rBtn = revButton[i];
     
 rBtn.addEventListener("click", function(e) {
 e.preventDefault();
-const revOverlay = revBlock.nextElementSibling;
+
 
  revOverlay.classList.add('reviews__overlay-active');
 
@@ -359,3 +417,271 @@ $(function() {
     })
 
 });
+
+//one page scroll
+class Slider {
+  constructor(data) {
+    this._length = data.length;
+    this._curSlide = 0;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get curSlide() {
+    return this._curSlide;
+  }
+
+  set curSlide(curSlide) {
+    this._curSlide = curSlide;
+    this.setCurSlideHandler(this);
+  }
+
+  nextSlide() {
+    if (this.curSlide < this.length - 1) {
+      this.curSlide++;
+      this.slideHandler(this);
+    }
+
+    return this.curElement;
+  }
+
+  prevSlide() {
+    if (this.curSlide) {
+      this.curSlide--;
+      this.slideHandler(this);
+    }
+
+    return this.curSlide;
+  }
+
+  slideTo(num) {
+    this.curSlide = num;
+    this.slideHandler(this);
+  }
+
+  slideHandler(slider) {
+    return slider;
+  }
+
+  setCurSlideHandler(slider) {
+    return slider;
+  }
+}
+
+const initOnePageScroll = () => {
+  const UP_KEY_CODE = 38;
+  const DOWN_KEY_CODE = 40;
+
+  const wrapper = $('.wrapper');
+  const content = wrapper.find('.maincontent');
+  const sections = content.find('.section');
+  const count = sections.length;
+  const links = content.find('a[data-section]');
+  const switcher = $('.section-switch');
+  const switches = switcher.find('.section-switch__item');
+  const offsets = sections.map((index, section) => $(section).offset().top);
+  const duration = +content.css('transition-duration').slice(0, -1) * 1000;
+  const arrowDown = $('.banner__arrow');
+
+  const slider = new Slider({
+    length: sections.length
+  });
+
+  const makeItemActive = (items, className, index) => {
+    items
+      .eq(index)
+      .addClass(`${className}--active`)
+      .siblings()
+      .removeClass(`${className}--active`);
+  };
+
+  slider.setCurSlideHandler = function() {
+    makeItemActive(sections, 'section', this.curSlide);
+    makeItemActive(switches, 'section-switch__item', this.curSlide);
+  };
+
+  const performTransition = index => {
+    if ($(window).height() >= 750) {
+      content.css('transform', 'translateY(' + -100 * index + '%)');
+    } else {
+      const offset = sections.eq(index).offset().top;
+
+      $('html').animate(
+        {
+          scrollTop: sections.eq(index).offset().top
+        },
+        1000
+      );
+    }
+  };
+
+  slider.slideHandler = function() {
+    performTransition(this.curSlide);
+  };
+
+  const translateHandler = evt => {
+    const deltaY = evt.originalEvent.deltaY;
+    const direction = deltaY > 0 ? 'down' : 'up';
+    const transitionendHandler = () => {
+      wrapper.on('wheel', translateHandler);
+      content.off('transitionend', transitionendHandler);
+    };
+
+    switch (direction) {
+      case 'down': {
+        if (slider.curSlide !== slider.length - 1) {
+          wrapper.off('wheel', translateHandler);
+          content.on('transitionend', transitionendHandler);
+        }
+        slider.nextSlide();
+        break;
+      }
+      case 'up': {
+        if (slider.curSlide) {
+          wrapper.off('wheel', translateHandler);
+          content.on('transitionend', transitionendHandler);
+        }
+        slider.prevSlide();
+      }
+    }
+  };
+
+  const keydownHandler = evt => {
+    const transitionendHandler = () => {
+      $(document).on('keydown', keydownHandler);
+      content.off('transitionend', transitionendHandler);
+    };
+
+    switch (evt.keyCode) {
+      case DOWN_KEY_CODE: {
+        if (slider.curSlide !== slider.length - 1) {
+          $(document).off('keydown', keydownHandler);
+          content.on('transitionend', transitionendHandler);
+        }
+        slider.nextSlide();
+        break;
+      }
+      case UP_KEY_CODE: {
+        if (slider.curSlide) {
+          $(document).off('keydown', keydownHandler);
+          content.on('transitionend', transitionendHandler);
+        }
+        slider.prevSlide();
+      }
+    }
+  };
+
+  const scrollHandler = () => {
+    const scrollTop = $('html').scrollTop();
+    let index = 0;
+
+    for (let i = 1; i < offsets.length; i++) {
+      if (scrollTop - offsets[i] >= 0) {
+        index = i;
+      } else {
+        break;
+      }
+    }
+
+    slider.curSlide = index;
+  };
+
+  if ($(window).height() >= 750) {
+    wrapper.on('wheel', translateHandler);
+    wrapper.on('touchmove', evt => evt.preventDefault());
+    $(document).on('keydown', keydownHandler);
+  } else {
+    $(document).on('scroll', scrollHandler);
+  }
+
+  if ($(window).width() <= 768) {
+    const BASE_DELTA_Y = 50;
+    let startY;
+    const touchEndHandler = evt => {
+      const deltaY = +evt.originalEvent.changedTouches[0].clientY - startY;
+      let direction;
+
+      if (deltaY > BASE_DELTA_Y) {
+        direction = 'up';
+      } else if (deltaY < -BASE_DELTA_Y) {
+        direction = 'down';
+      } else {
+        return;
+      }
+
+      const transitionendHandler = () => {
+        $(window).on('touchend', touchEndHandler);
+        content.off('transitionend', transitionendHandler);
+      };
+
+      switch (direction) {
+        case 'down': {
+          if (slider.curSlide !== slider.length - 1) {
+            $(window).off('touchend', touchEndHandler);
+            content.on('transitionend', transitionendHandler);
+          }
+          slider.nextSlide();
+          break;
+        }
+        case 'up': {
+          if (slider.curSlide) {
+            $(window).off('touchend', touchEndHandler);
+            content.on('transitionend', transitionendHandler);
+          }
+          slider.prevSlide();
+        }
+      }
+    };
+
+    $(window).on('touchstart', evt => {
+      startY = +evt.originalEvent.changedTouches[0].clientY;
+    });
+
+    $(window).on('touchend', touchEndHandler);
+  }
+
+  const clickLinkHandler = evt => {
+    evt.preventDefault();
+
+    slider.slideTo(parseInt($(evt.target).attr('data-section'), 10));
+
+    if ($(window).height() >= 750) {
+      links.off('click', clickLinkHandler);
+
+      const transitionendHandler = () => {
+        links.on('click', clickLinkHandler);
+        content.off('transitionend', transitionendHandler);
+      };
+
+      content.on('transitionend', transitionendHandler);
+    }
+  };
+  const clickSwitchHandler = evt => {
+    evt.preventDefault();
+
+    slider.slideTo($(evt.target).index());
+
+    if ($(window).height() >= 750) {
+      switches.off('click', clickSwitchHandler);
+
+      const transitionendHandler = () => {
+        switches.on('click', clickSwitchHandler);
+        content.off('transitionend', transitionendHandler);
+      };
+
+      content.on('transitionend', transitionendHandler);
+    }
+  };
+
+  links.on('click', clickLinkHandler);
+  switches.on('click', clickSwitchHandler);
+  arrowDown.on('click', () => slider.slideTo(1));
+
+  if ($(window).height() >= 750) {
+    wrapper.addClass('wrapper--active');
+  }
+};
+
+export default initOnePageScroll;
